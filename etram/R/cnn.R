1a6ad6b8e6c1b78d3fc92750fc0a8375e2481038
Andrea <andrea.goetschi@uzh.ch>
2022-03-17 17:21:32 +0100

    add cnn for mela

--- etram/R/cnn.R
+++ etram/R/cnn.R
@@ -67,7 +67,7 @@ cnn_mnist <- function(output_shape = NULL, mbl = FALSE, ll_activation = "linear"
 cnn_stroke <- function(output_shape = NULL, mbl = FALSE, ll_activation = "linear",
                        dropout_rate = 0.3, ll_bias = FALSE, input_shape = dim(im)[2:5], ...) {
 
-  mod <- keras_model_sequential() %>%
+  m <- keras_model_sequential() %>%
     layer_conv_3d(input_shape = input_shape, filters = 32, # 32
                   kernel_size = c(3, 3, 3), padding = "same",
                   activation = "relu") %>%
@@ -92,5 +92,41 @@ cnn_stroke <- function(output_shape = NULL, mbl = FALSE, ll_activation = "linear
       layer_trafo_intercept()(.)
     } else .
     }
-  return(mod)
+  return(m)
 }
+
+#' CNN for melanoma data
+#' @export
+cnn_melanoma <- function(output_shape = NULL, mbl = FALSE, ll_activation = "linear",
+                         dropout_rate = 0.3, ll_bias = FALSE, input_shape = dim(im)[2:5], ...) {
+
+  m <- keras_model_sequential() %>%
+    layer_conv_2d(filters = 16, kernel_size = c(3, 3), activation = "relu",
+                  input_shape = input_shape, padding = "same") %>%
+    layer_dropout(dropout_rate) %>%
+    layer_conv_2d(filters = 16, kernel_size = c(3, 3), activation = "relu",
+                  padding = "same") %>%
+    layer_dropout(dropout_rate) %>%
+    layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+    layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+                  padding = "same") %>%
+    layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+    layer_dropout(dropout_rate) %>%
+    layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+                  padding = "same") %>%
+    layer_dropout(dropout_rate) %>%
+    layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+
+    layer_flatten() %>%
+    layer_dense(units = 256, activation = "relu") %>%
+    layer_dropout(dropout_rate) %>%
+    layer_dense(units = 64, activation = "relu") %>%
+    layer_dropout(dropout_rate) %>%
+    layer_dense(output_shape, activation = ll_activation, use_bias = ll_bias, ... = ...) %>%
+    {if (mbl){
+      layer_trafo_intercept()(.)
+    } else .
+    }
+  return(m)
+}
+
