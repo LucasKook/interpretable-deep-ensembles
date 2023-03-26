458a3786196225aec58fcf11c97117cba42d8c2e
lkook <lucasheinrich.kook@uzh.ch>
2023-03-26 15:09:32 +0200

    nop

---
+++ scratch/deeptrafo-continuous-example.R
@@ -0,0 +1,15 @@
+
+library("deeptrafo")
+
+missp <- function(n = 100) {
+  x <- runif(n, min = -4, max = 4)
+  y <- 3 * plogis(3 * x) - rnorm(n, sd = 0.6^2)
+  y <- y - mean(y)
+  data.frame(y = y, x = x)
+}
+
+d <- missp(1e3)
+
+m <- BoxCoxNN(y | x ~ 1, data = d, optimizer = optimizer_adam(1e-1, decay = 1e-4))
+(ens <- ensemble(m, epochs = 100, verbose = TRUE, validation_split = 0.1))
+plot(ens, type = "trafo", newdata = data.frame(x = 0))
