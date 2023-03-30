9ef43603921c988e140e8d3e7e3b332d700ab6f2
lkook <lucasheinrich.kook@uzh.ch>
2023-03-30 14:56:17 +0200

    baselines

--- benchmark/baseline.R
+++ benchmark/baseline.R
@@ -7,7 +7,7 @@ set.seed(24101968)
 
 library("caret")
 library("parallel")
-library("deeptrafo")
+library("tram")
 library("tidyverse")
 library("xtable")
 
@@ -18,7 +18,7 @@ nfolds <- 5
 nens <- 5
 nep <- 1e4
 
-odir <- file.path("benchmark", "results", make.names(Sys.time()))
+odir <- file.path("benchmark", "results", "baseline")
 
 ### Create output directory
 if(!dir.exists(odir))
@@ -42,8 +42,8 @@ data_reader <- function(
              "ltfsid", "naval_compressor", "naval_turbine", "real", "wine",
              "yacht")
 ) {
-    name <- match.arg(name)
-    read.table(paste0("benchmark/benchmark_data/", name, ".data"))
+  name <- match.arg(name)
+  read.table(paste0("benchmark/benchmark_data/", name, ".data"))
 }
 
 ### Benchmark
@@ -76,36 +76,24 @@ benchmark_per_dataset <- function(name, folds = nfolds){
     testX <- X[testind,]
     testY <- if (name == "wine") y[testind] else as.numeric(y[testind])
 
-    dtrain <- list(y = trainY, x = trainX)
-    dtest <- list(y = testY, x = testX)
+    dtrain <- data.frame(y = trainY, x = trainX)
+    dtest <- data.frame(y = testY, x = testX)
 
-    # Define neural network architecture
-    nn <- \(x) x %>%
-      layer_dense(input_shape = ncol(trainX), units = 16L, activation = "relu") %>%
-      layer_dense(input_shape = ncol(trainX), units = 16L, activation = "relu") %>%
-      layer_dense(input_shape = ncol(trainX), units = 8L, activation = "relu") %>%
-      layer_dense(1L)
+    if (name == "wine") {
+      m0 <- Polr(y ~ 1, data = dtrain)
+      m <- Polr(y ~ ., data = dtrain)
+    } else {
+      m0 <- Colr(y ~ 1, data = dtrain, prob = c(0, 1), order = 10)
+      m <- Colr(y ~ ., data = dtrain, prob = c(0, 1), order = 10)
+    }
 
-    # Fit transformation ensemble with callbacks
-    ens <- trafoensemble(
-      y ~ nn(x), data = dtrain, list_of_deep_models = list(nn = nn),
-      validation_split = 0.1, optimizer = optimizer_adam(1e-3, decay = 1e-4),
-      callbacks = list(callback_early_stopping(patience = 50, restore_best_weights = TRUE)),
-      epochs = nep, n_ensemble = nens, seed = sample.int(1e6, 5), verbose = TRUE
-    )
-
-    lltrain <- logLik(ens, newdata = dtrain, convert_fun = mean)
-    preds <- do.call("cbind", predict(ens, newdata = dtrain, type = "pdf"))
-    lltrain$classical <- -mean(log(apply(preds, 1, mean)))
-
-    lltest <- logLik(ens, newdata = dtest, convert_fun = mean)
-    test_preds <- do.call("cbind", predict(ens, newdata = dtest, type = "pdf"))
-    lltest$classical <- -mean(log(apply(test_preds, 1, mean) + 1e-16))
+    lltrain <- c(m0 = - logLik(m0) / nrow(dtrain), m = - logLik(m) / nrow(dtrain))
+    lltest <- c(m0 = - logLik(m0, newdata = dtest) / nrow(dtest),
+                m = - logLik(m, newdata = dtest) / nrow(dtest))
 
     # Extract NLL from train and test data
     res_fold_i <- suppressMessages(suppressWarnings(
-      data.frame(cbind(train = unlist(lltrain),
-                       test = unlist(lltest))) %>%
+      data.frame(cbind(train = lltrain, test = lltest)) %>%
         rownames_to_column("ens")
     ))
 
@@ -133,9 +121,9 @@ tab_raw <- do.call("rbind", lapply(seq_along(lf), function(i) {
     group_by(ens) %>%
     summarize_at(c("train", "test"), list(mean = mean, sd = sd))
   dftrain <- as.data.frame(t(paste0(signif(sumtab$train_mean, ndigits), " (",
-                               signif(sumtab$train_sd, ndigits), ")")))
+                                    signif(sumtab$train_sd, ndigits), ")")))
   dftest <- as.data.frame(t(paste0(signif(sumtab$test_mean, ndigits), " (",
-                               signif(sumtab$test_sd, ndigits), ")")))
+                                   signif(sumtab$test_sd, ndigits), ")")))
   rownames(dftrain) <- rownames(dftest) <- gsub("(.*)\\.RDS", "\\1", lf[i])
   colnames(dftrain) <- colnames(dftest) <- sumtab$ens
   dftest
